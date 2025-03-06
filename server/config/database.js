5d5a6558b44ada0a43327ad6a1e494bcd8779047
Paul Paumier Martinez <manaweb@posteo.net>
2025-03-07 00:14:40 +0100

    replaced db username

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const sequelize = new Sequelize('TabTrackr', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+// archie: const sequelize = new Sequelize('TabTrackr', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('TabTrackr', 'nour', '', {
     host: 'localhost',
     dialect: 'postgres',
     logging: false,
